c28aecc4bf3bfb2606bdbb1a663303fd0780ecdf
Guilherme Ferreira <gui_henrique09@hotmail.com>
2023-12-06 18:17:26 -0300

    Attt

---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+document.getElementById('meuBotao').addEventListener('click', function() {
+    alert('Bem-vindo ao Mundo Bruxo de Harry Potter! Explore nosso site para descobrir mais sobre as casas, feitiços, personagens e relíquias do mundo mágico.');
+  });
